587023213cd56bf859a5a9d72d0d58d38477d615
Kishor-J-K <Kishor-J-K@users.noreply.github.com>
2026-08-11 15:53:32 +0530

    Wrote code to display on home

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
+from todo.models import Task
 def home(request):
-    return render(request, 'home.html', {'title': 'Home'})
+    task = Task.objects.filter(is_completed=False)
+    return render(request, 'home.html', {'tasks': task})
