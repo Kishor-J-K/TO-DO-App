f6312cc86e2904dfd52358febdc23283ea7734bb
Kishor-J-K <Kishor-J-K@users.noreply.github.com>
2026-08-14 22:59:06 +0530

    Project completed

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('addTask/', views.add_task, name='add_task'),
+    path('mark_as_done/<int:id>/', views.mark_as_done, name='mark_as_done'),
+    path('delete_task/<int:id>/', views.delete_task, name='delete_task'),
+    path('edit_task/<int:id>/', views.edit_task, name='edit_task'),
+    path('mark_as_undone/<int:id>/', views.mark_as_undone, name='mark_as_undone'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
 from todo.models import Task
 def home(request):
-    task = Task.objects.filter(is_completed=False).order_by('-created_at')
-    completed_task = Task.objects.filter(is_completed=True).order_by('-updated_at')
-    return render(request, 'home.html', {'tasks': task, 'completed_tasks': completed_task})
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
+    context = {
+        'tasks': tasks,
+        'completed_tasks': completed_tasks
+    }
+    return render(request, 'home.html', context)
+
+def add_task(request):
+    task = request.POST['task_name']
+    Task.objects.create(task_name=task)
+    return redirect('home')
+
+def mark_as_done(request, id):
+    task = Task.objects.get(id=id)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def delete_task(request, id):
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect('home')
+
+def edit_task(request, id):
+    task = Task.objects.get(id=id)
+    if request.method == 'POST':
+        task.task_name = request.POST['task_name']
+        task.save()
+        return redirect('home')
+    context = {
+        'task': task
+    }
+    return render(request, 'edit_task.html', context)
+
+def mark_as_undone(request, id):
+    task = Task.objects.get(id=id)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
