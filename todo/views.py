7128a1315abd8385099b9b96a22328d95b090c84
Kishor-J-K <Kishor-J-K@users.noreply.github.com>
2026-08-14 19:13:47 +0530

    List of completed tasks

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from todo.models import Task
 def home(request):
-    task = Task.objects.filter(is_completed=False)
-    return render(request, 'home.html', {'tasks': task})
+    task = Task.objects.filter(is_completed=False).order_by('-created_at')
+    completed_task = Task.objects.filter(is_completed=True).order_by('-updated_at')
+    return render(request, 'home.html', {'tasks': task, 'completed_tasks': completed_task})
